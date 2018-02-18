b23d727d631cefbb6169e50ff5b3db5542365c24
Farhood Fazeli <farhood.fazeli@gmail.com>
2018-02-18 18:20:23 +0100

    Changed buttonCount var to test out first commit via gitkraken

--- Nick/ViewController.swift
+++ Nick/ViewController.swift
@@ -9,10 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var buttonCount = 1
 
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        buttonCount = buttonCount + 1
+        
+        print(buttonCount)
+        
+        if buttonCount == 10 {
+        
+            view.backgroundColor = UIColor.black
+            
+            myLabel.textColor = UIColor.white
+            
+            myLabel.text = "Welcome to your first million"
+            
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
