7df270a444198af283d7cdb60dcd8d4ff2a3f306
Unknown <farhood.fazeli@gmail.com>
2018-02-22 22:06:08 +0100

    Added boolean if else switch

--- Nick/ViewController.swift
+++ Nick/ViewController.swift
@@ -11,12 +11,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var buttonCount = 1
+    // var buttonCount = 1
 
     @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var additionSwitch: UISwitch!
     
     @IBAction func buttonTapped(_ sender: Any) {
         
+        let addition = additionSwitch.isOn
+        
+        if addition {
+            
+            let sum = Int(topTextField.text!)! + Int(bottomTextField.text!)!
+            
+            myLabel.text = "\(topTextField.text!) + \(bottomTextField.text!) = \(sum)"
+            
+        } else {
+            
+            let sum = Int(topTextField.text!)! - Int(bottomTextField.text!)!
+            
+            myLabel.text = "\(topTextField.text!) - \(bottomTextField.text!) = \(sum)"
+            
+        }
+        
+
+        
+        /*
         buttonCount = buttonCount + 1
         
         print(buttonCount)
@@ -29,8 +51,8 @@ class ViewController: UIViewController {
             
             myLabel.text = "Welcome to your first million"
             
-        }
-        
+        }   */
+ 
     }
     
     override func viewDidLoad() {
